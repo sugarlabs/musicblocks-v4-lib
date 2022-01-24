6ba148192add6cc8be26ddce6e00cd503e7c3895
Anindya Kundu <anindyaak007@gmail.com>
2022-01-24 23:17:42 +0530

    chore: remove archived source code

--- src/archive/syntax-core/@types/symbolTable.d.ts
+++
@@ -1,8 +0,0 @@
-import { TBoolean, TString } from '../primitiveElements';
-import { TPrimitive, TPrimitiveName } from './primitiveTypes';
-
-export interface ISymbolTable {
-    symbolExists: (symbol: TString) => TBoolean;
-    addSymbol: (symbol: TString, data: TPrimitive) => void;
-    getSymbolData: (symbol: TString) => TPrimitive;
-}
--- src/archive/syntax-core/context.ts
+++
@@ -1,5 +0,0 @@
-import { IContext } from './@types/context';
-
-export class Context implements IContext {
-    constructor() {}
-}
